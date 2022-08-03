d75cdbe7f30a760b5cf37507728501a02a5656c2
mingthean.lay <mingthean.lay@gmail.com>
2022-08-03 20:05:57 +0700

    fix: ignore cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./src/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 };

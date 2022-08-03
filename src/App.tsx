450be1afad216ab5710d6c0e4837749564786647
mingthean.lay <mingthean.lay@gmail.com>
2022-08-03 17:04:31 +0700

    fix: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-function App() {
-  return (
-    <div className="flex justify-center h-screen items-center">
-      <h1 className="font-bold text-7xl text-center md:text-8xl px-4">
-        React + Vite + TailwindCSS
-      </h1>
-    </div>
-  );
-}
+const App = () => (
+  <div className="flex justify-center h-screen items-center">
+    <h1 className="font-bold text-7xl text-center md:text-8xl px-4">React + Vite + TailwindCSS</h1>
+  </div>
+);
 
 export default App;
